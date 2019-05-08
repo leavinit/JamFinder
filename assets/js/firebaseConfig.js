89dc2155cc715d36bcb67abf1dde10761ca657d4
Adam S <leavinit@gmail.com>
2019-05-08 14:48:33 -0500

    Initial commit w/o div structure

---
+++ assets/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDDi3tdOPCNlBKwFbhxlcNCSzX7cgkgB0g",
+    authDomain: "findyourjam-759e9.firebaseapp.com",
+    databaseURL: "https://findyourjam-759e9.firebaseio.com",
+    projectId: "findyourjam-759e9",
+    storageBucket: "findyourjam-759e9.appspot.com",
+    messagingSenderId: "64756213731",
+    appId: "1:64756213731:web:72a53d7645077834"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
